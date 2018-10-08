b190dd64441d14e57ca6482d2507bc2743aeddc3
yangtongxi <yangtongxi@37.com>
2018-10-08 18:15:55 +0800

    学习基于netty的简单后端通讯---客户端

---
+++ mmorpg-client/src/main/java/com/client/Client.java
@@ -0,0 +1,88 @@
+package com.client;
+
+import java.net.InetSocketAddress;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import com.common.codc.RequestEncoder;
+import com.common.codc.ResponseDecoder;
+import com.module.player.packet.PlayerLoginRequest;
+import com.server.handler.ClientHandler;
+import com.common.model.Request;
+import org.jboss.netty.bootstrap.ClientBootstrap;
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelFuture;
+import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.channel.Channels;
+import org.jboss.netty.channel.socket.nio.NioClientSocketChannelFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * 客户端
+ *
+ */
+
+@Service
+public class Client {
+
+    public static void clientStart(){
+        //服务类
+        ClientBootstrap bootstrap = new  ClientBootstrap();
+
+        //线程池
+        ExecutorService boss = Executors.newCachedThreadPool();
+        ExecutorService worker = Executors.newCachedThreadPool();
+
+        //socket工厂
+        bootstrap.setFactory(new NioClientSocketChannelFactory(boss, worker));
+
+        //管道工厂
+        bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
+
+            @Override
+            public ChannelPipeline getPipeline() throws Exception {
+                ChannelPipeline pipeline = Channels.pipeline();
+                pipeline.addLast("decoder", new ResponseDecoder());
+                pipeline.addLast("encoder", new RequestEncoder());
+                pipeline.addLast("clientHandler", new ClientHandler());
+                return pipeline;
+            }
+        });
+
+        //连接服务端
+        ChannelFuture connect = bootstrap.connect(new InetSocketAddress("127.0.0.1", 12345));
+        Channel channel = null;
+        try {
+            channel = connect.sync().getChannel();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("client start");
+
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+
+            System.out.println("请输入玩家id:");
+            long playerId=Long.parseLong(scanner.nextLine());
+            System.out.println("请输入密码：");
+            String pass=scanner.nextLine();
+
+            PlayerLoginRequest playerLoginRequest=new PlayerLoginRequest();
+            playerLoginRequest.setPlayerId(playerId);
+            playerLoginRequest.setPass(pass);
+
+            //将数据封装
+            Request request=new Request();
+            request.setModule((short)1);
+            request.setCmd((short)2);
+            request.setData(playerLoginRequest.getBytes());
+            //发送请求
+            channel.write(request);
+        }
+    }
+
+}
+
---
+++ mmorpg-client/src/main/java/com/server/handler/ClientHandler.java
@@ -0,0 +1,80 @@
+package com.server.handler;
+
+import com.common.model.Response;
+import com.module.player.packet.PlayerLoginResponse;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.ChannelStateEvent;
+import org.jboss.netty.channel.ExceptionEvent;
+import org.jboss.netty.channel.MessageEvent;
+import org.jboss.netty.channel.SimpleChannelHandler;
+import org.springframework.stereotype.Service;
+
+/**
+ * 消息接受处理类
+ *
+ */
+@Service
+public class ClientHandler extends SimpleChannelHandler {
+
+	/**
+	 * 接收消息
+	 */
+	@Override
+	public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) throws Exception {
+			Response message = (Response)e.getMessage();
+
+			if(message.getModule() == 1){
+				
+				if(message.getCmd() == 1){
+					
+				}else if(message.getCmd() == 2){
+
+					PlayerLoginResponse playerLoginResponse=new PlayerLoginResponse();
+					playerLoginResponse.readFromBytes(message.getData());
+
+					System.out.println("登录成功奖励金币："+playerLoginResponse.getMoney());
+					
+				}
+				
+			}else if (message.getModule() != 1){
+				
+				
+			}
+	}
+
+	/**
+	 * 捕获异常
+	 */
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) throws Exception {
+		System.out.println("exceptionCaught");
+		super.exceptionCaught(ctx, e);
+	}
+
+	/**
+	 * 新连接
+	 */
+	@Override
+	public void channelConnected(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
+		System.out.println("channelConnected");
+		super.channelConnected(ctx, e);
+	}
+
+	/**
+	 * 必须是链接已经建立，关闭通道的时候才会触发
+	 */
+	@Override
+	public void channelDisconnected(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
+		System.out.println("channelDisconnected");
+		super.channelDisconnected(ctx, e);
+	}
+
+	/**
+	 * channel关闭的时候触发
+	 */
+	@Override
+	public void channelClosed(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
+		System.out.println("channelClosed");
+		super.channelClosed(ctx, e);
+	}
+}
