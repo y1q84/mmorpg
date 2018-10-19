c599545d121051b4ceb565633e3dfb42d25cf7bb
yangtongxi <yangtongxi@37.com>
2018-10-19 22:25:08 +0800

    处理客户端发送过来的消息

--- mmorpg-server/src/main/java/com/framework/commnunication/server/websocket/handler/GameWebSocketServerHandler.java
+++ mmorpg-server/src/main/java/com/framework/commnunication/server/websocket/handler/GameWebSocketServerHandler.java
@@ -1,8 +1,9 @@
-package com.framework.commnunication.server.handler;
+package com.framework.commnunication.server.websocket.handler;
 
 import com.baidu.bjf.remoting.protobuf.Codec;
 import com.common.packetId.AbstractPaket;
 import com.common.packetId.PacketId;
+import com.common.packetId.impl.ReqCommandPacket;
 import com.module.logic.gm.manager.GMManager;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.Channel;
@@ -59,29 +60,46 @@ public class GameWebSocketServerHandler extends SimpleChannelInboundHandler<WebS
              * 包长 + packetId + 数据
              * 包长4个字节，packetId占2个字节
              **/
-            int packetLength=byteBuf.readInt();//读取包长
+            /**读取包长**/
+            int packetLength=byteBuf.readInt();
             logger.info("包长为："+packetLength);
             if ((packetLength-4)!=byteBuf.readableBytes()){
                 logger.error("该包数据不完整...");
                 return ;
             }
-            short pacId=byteBuf.readShort();//读取packetId
+            /**读取packetId**/
+            short pacId=byteBuf.readShort();
             logger.info("获取到packetId为："+pacId);
-            byte[] data=new byte[byteBuf.readableBytes()];//读取数据
+            /**读取数据**/
+            byte[] data=new byte[byteBuf.readableBytes()];
+            byteBuf.readBytes(data);
+            //输出字节数组内容
+            StringBuilder stringBuilder=new StringBuilder();
+            for (byte b : data) {
+                //stringBuilder.append(new Integer(b & 0xFF)+",");
+                //将byte转为无复数的byte
+                stringBuilder.append(Byte.toUnsignedInt(b)+",");
+            }
+            logger.info(String.format("字节数组内容为:[%s]",stringBuilder.toString()));
 
+            /**发送过来的数据应该是一个封装的RequestPacket对象**/
 
-            //根据packetId获取jprotobuf的编解码器
-//			Codec codec=packetId.getCodec(pacId);
-//			//将数据进行解码
-//			Object obj=codec.decode(data);
-//			AbstractPaket absPacket=(AbstractPaket)obj;
-//
-//			if(obj instanceof AbstractPaket){
-//				logger.info("收到的对象为；"+absPacket);
-//			}
-//
-//			//将请求传给下一个handler处理
-//			ctx.fireChannelRead(absPacket);
+            /**根据packetId获取jprotobuf的编解码器**/
+			Codec codec=packetId.getCodec(pacId);
+			logger.info("packeId对应的编解码器："+codec);
+			/**将数据进行解码**/
+			Object obj=codec.decode(data);
+
+			if(obj instanceof ReqCommandPacket){
+			    channel.writeAndFlush(new TextWebSocketFrame("登陆成功"));
+				logger.info("moveId:"+((ReqCommandPacket) obj).getmoveId());
+			}
+
+			//将请求传给下一个handler处理
+            if(obj instanceof AbstractPaket){
+                ctx.fireChannelRead(obj);
+            }
+
 
 
 //            RequestPacket requestPacket=RequestPacket.valueOf(packetId2,data);
@@ -105,6 +123,7 @@ public class GameWebSocketServerHandler extends SimpleChannelInboundHandler<WebS
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
         Channel incoming = ctx.channel();
+//        incoming.writeAndFlush(new TextWebSocketFrame("握手成功..."));
         logger.info("用户:"+incoming.remoteAddress()+"上线...");
     }
 
@@ -129,6 +148,7 @@ public class GameWebSocketServerHandler extends SimpleChannelInboundHandler<WebS
         super.userEventTriggered(ctx, evt);
         if(evt instanceof WebSocketServerProtocolHandler.HandshakeComplete){
             logger.info("握手成功...");
+            ctx.channel().writeAndFlush("连接成功");
         }
     }
 }
