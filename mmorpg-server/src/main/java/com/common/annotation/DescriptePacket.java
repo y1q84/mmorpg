c0aff17c647b00e24160e4e49a488c3f7ea09aeb
yangtongxi <yangtongxi@37.com>
2018-10-22 21:33:06 +0800

    增加描述包信息的注解

--- mmorpg-server/src/main/java/com/common/annotation/DescriptePacket.java
+++ mmorpg-server/src/main/java/com/common/annotation/DescriptePacket.java
@@ -5,5 +5,6 @@ import java.lang.annotation.*;
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-public @interface WsClass {
+public @interface DescriptePacket {
+    String description() default "";
 }
--- mmorpg-server/src/main/java/com/common/annotation/WsPacket.java
+++ mmorpg-server/src/main/java/com/common/annotation/WsPacket.java
@@ -1,13 +1,10 @@
 package com.common.annotation;
 
-import org.springframework.stereotype.Component;
-
 import java.lang.annotation.*;
 
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@Component
 public @interface WsPacket {
     short packetId();
 }
