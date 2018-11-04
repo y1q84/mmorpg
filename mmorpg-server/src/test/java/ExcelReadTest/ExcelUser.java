d9c876f3355634974bd6057a58e32058cca7785a
tom <tom@123.com>
2018-11-05 00:57:53 +0800

    测试从Excel中读取数据

---
+++ mmorpg-server/src/test/java/ExcelReadTest/ExcelUser.java
@@ -0,0 +1,45 @@
+package ExcelReadTest;
+
+/**
+ * author:ydx
+ * create 2018\11\3 0003
+ */
+public class ExcelUser {
+    private String name;
+    private Integer age;
+    private String  address;
+    private Character sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Character getSex() {
+        return sex;
+    }
+
+    public void setSex(Character sex) {
+        this.sex = sex;
+    }
+}
+
