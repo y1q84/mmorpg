d2f27858ae48c3ffa7917eafbc4de55f861c2f3d
tom <tom@123.com>
2018-11-07 01:10:40 +0800

    增加读取资源管理类

--- mmorpg-server/src/main/java/com/common/resource/read/ReadJson.java
+++ mmorpg-server/src/main/java/com/common/resource/read/ReadJson.java
@@ -3,9 +3,11 @@ package com.common.resource.read;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
 import com.common.resource.data.ResourceDataObject;
+import com.common.resource.type.ResourceType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
 
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
@@ -14,6 +16,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Component
 public class ReadJson implements ReadResource {
 
     Logger logger=LoggerFactory.getLogger(ReadJson.class);
@@ -46,6 +49,11 @@ public class ReadJson implements ReadResource {
             }
         }
         return list;
+    }
+
+    @Override
+    public ResourceType getResourceType() {
+        return ResourceType.MAP;
     }
 
 //    public <T, E> Map<T,E> read2(ResourceDataObject resourceDataObject) {
---
+++ mmorpg-server/src/main/java/com/common/resource/read/ReadManager.java
@@ -0,0 +1,58 @@
+package com.common.resource.read;
+
+import com.common.resource.data.ResourceDataObject;
+import com.common.resource.type.ResourceType;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * author:ydx
+ * create 2018\11\6 0006
+ */
+@Component
+public class ReadManager implements ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+    private Map<ResourceType,ReadResource> type2Resource=new HashMap<>();
+
+    @PostConstruct
+    public void init(){
+        Map<String,ReadResource> map=applicationContext.getBeansOfType(ReadResource.class);
+        map.forEach((str,reader)->{
+           type2Resource.put(reader.getResourceType(),reader);
+        });
+        valueOf();
+    }
+
+    public static ReadManager valueOf(){
+        return new ReadManager();
+    }
+
+    public ReadResource getReadResource(ResourceType type){
+        return type2Resource.get(type);
+    }
+
+    public <T> List<T> read(ResourceDataObject resourceDataObject){
+        ReadResource readResource=getReadResource(resourceDataObject.getResourceType());
+        return readResource.read(resourceDataObject);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("调用了...");
+        this.applicationContext=applicationContext;
+    }
+
+    public static void main(String[] args) {
+        ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+        ReadManager manager=context.getBean(ReadManager.class);
+    }
+}
