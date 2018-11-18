50306b97e2dee33d7ab8df3ca61e92858ee9cb7a
tom <tom@123.com>
2018-11-19 00:54:58 +0800

    将集合类型转为json格式存进数据库

--- mmorpg-server/src/main/java/com/common/persist/IEntity.java
+++ mmorpg-server/src/main/java/com/common/persist/IEntity.java
@@ -1,9 +1,16 @@
 package com.common.persist;
 
+import com.common.util.JsonStringType;
+import org.hibernate.annotations.TypeDef;
+
+import javax.persistence.MappedSuperclass;
+
 /**
  * author:ydx
  * create 2018\11\15 0015
  */
+@MappedSuperclass
+@TypeDef(name = "json", typeClass = JsonStringType.class)
 public interface IEntity<ID> {
 
     ID getId();
